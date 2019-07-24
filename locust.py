ca9745e7c39ea4a32cf7de0d8571ed9afe53d372
Patrick Vogel <patrickvogel@live.nl>
2019-07-24 12:05:52 +0200

    updated locust

--- locust.py
+++ locust.py
@@ -1,14 +1,27 @@
 from locust import task, TaskSequence
+import os
+
+load = 10
 
 
 class UserBehavior(TaskSequence):
-
     @task
     def index(self):
         self.client.get('/')
 
 
+class LoadBehavior(TaskSequence):
+    @task
+    def with_load(self):
+        global load
+        load *= 2
+        self.client.get(f'/with_load?load={load}')
+
+
 class WebsiteUser(HttpLocust):
-    task_set = UserBehavior
+    if os.environ.get('TYPE', 'USER') == 'LOAD':
+        task_set = LoadBehavior
+    else:
+        task_set = UserBehavior
     min_wait = 0
     max_wait = 0
